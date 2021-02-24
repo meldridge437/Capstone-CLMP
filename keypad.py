adac6a0ff84903cf397e2e14c0ebfd77c9889d5a
meldridge437 <37875191+meldridge437@users.noreply.github.com>
2021-02-24 13:36:47 -0600

    beta keypad script with function calls
    may need to create a "setup" function unless we use this by "from keypad import *"

---
+++ keypad.py
@@ -0,0 +1,59 @@
+#################################################
+#
+#	source: https://github.com/adafruit/Adafruit_CircuitPython_MatrixKeypad
+#	Adapted By: Matthew Eldridge
+#	Project: Capstone Doorlock
+#
+#################################################
+
+import adafruit_matrixkeypad
+from digitalio import DigitalInOut
+import board
+
+# Classic 4x4 matrix keypad
+cols = [DigitalInOut(x) for x in (board.D0, board.D2, board.D4, board.D6)]
+rows = [DigitalInOut(x) for x in (board.D1, board.D3, board.D5, board.D7)]
+keys = ((1, 2, 3, 'R'),
+        (4, 5, 6, 'Y'),
+        (7, 8, 9, 'N'),
+        ('*', 0, '#', 'E'))
+''' letter buttons redefined meanings
+R - reset
+Y - yes
+N - no
+E - enter
+* - scan finger
+# - scan face
+'''
+keypad = adafruit_matrixkeypad.Matrix_Keypad(rows, cols, keys)
+keyPressed = keypad.pressed_keys
+
+################# Functions #######################
+
+def getPIN(lengthPIN=8):
+	pin = []
+	strPIN = ""
+	while True:
+		if(str(keyPressed).isdigit()):
+			pin.append(keyPressed)
+		if (len(pin) <= lengthPIN or keyPressed == 'E'):
+			break	
+	for n in pin:
+		strPIN += str(n)
+	return int(strPIN)
+
+def getLetterResponse():
+	while True:
+		if (!str(keyPressed).isdigit()):
+			return keyPressed
+		time.sleep(0.1)
+
+#################### MAIN #########################
+
+''' given sample for testing
+while True:
+    keys = keypad.pressed_keys
+    if keys:
+        print("Pressed: ", keys)
+    time.sleep(0.1)
+'''
