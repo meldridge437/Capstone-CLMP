e74c8d8181b2f90b033b289e9a918a178d156206
Matthew Eldridge <matthew.eldridge437@gmail.com>
2021-04-27 22:42:38 -0500

    modified db scripts for full update entry functionality

--- test_db.py
+++ test_db.py
@@ -1,5 +1,12 @@
 import db_client as db
 
-res = db.findInDB(["username","fingerID"],["pin"],[54321])
+#db.createNewDBEntry(["username","pin","fingerID"],["Peyton","1234","1"])
 
+# test to see whats there
+res = db.findInDB(["username"],["pin"],[32651])
 print(res)
+
+# update entry
+db.updateDBEntry(["username"],res,["pin", "fingerID"],[110100,60])
+res2 = db.findInDB(["username","pin","fingerID"],["fingerID"],[60])
+print(res2)
