0e1287f1010906d80e30e10758c64cf90047809b
Chris <chrisbouton99@gmail.com>
2021-05-04 22:14:35 -0500

    bluel led and speaker added

--- test_db.py
+++ test_db.py
@@ -1,12 +1,15 @@
 import db_client as db
 
-#db.createNewDBEntry(["username","pin","fingerID"],["Peyton","1234","1"])
+newUserData = ["NewDemo","9999","5"]
+fields = ["username","pin","fingerID"]
+
+db.createNewDBEntry(fields,newUserData)
 
 # test to see whats there
-res = db.findInDB(["username"],["pin"],[32651])
+res = db.findInDB(fields,["pin"],[newUserData[1]])
 print(res)
 
 # update entry
-db.updateDBEntry(["username"],res,["pin", "fingerID"],[110100,60])
-res2 = db.findInDB(["username","pin","fingerID"],["fingerID"],[60])
-print(res2)
+#db.updateDBEntry(["username"],res,["pin", "fingerID"],[110100,60])
+#res2 = db.findInDB(["username","pin","fingerID"],["fingerID"],[60])
+#print(res2)
